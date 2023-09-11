52f6a2a19876a4916aec498daa77554e93b61c5e
Clément FOSSORIER <clementfiesta@gmail.com>
2023-09-11 13:52:43 +0200

    First version of dall-e command

---
+++ src/commands/dall-e.js
@@ -0,0 +1,12 @@
+const dallEController = require('../controllers/dallEController');
+
+module.exports = {
+    name: 'dall-e',
+    run: async (client, message, args) => {
+        try {
+            message.channel.send(await dallEController.runPromptWithArgs(args));
+        } catch(error) {
+            console.error(error);
+        }
+    }
+}
---
+++ src/controllers/dallEController.js
@@ -0,0 +1,38 @@
+
+module.exports = {
+    async runPrompt(prompt) {
+        const options = {
+            method: 'POST',
+            headers: {
+                'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`,
+                'Content-Type': `application/json`
+            },
+            body: JSON.stringify({
+                'prompt': prompt
+            })
+        }
+        console.log(prompt);
+        try {
+            const response = await fetch('https://api.openai.com/v1/images/generations', options);
+            const data = await response.json();
+
+            console.log(data)
+
+            let message = '';
+            data?.data.forEach(imageObject => {
+                message = message + imageObject.url + '\n';
+            });
+
+            return message;
+        } catch(error) {
+            console.error(error);
+        }
+    },
+    async runPromptWithArgs(args) {
+        let prompt = '';
+        args.forEach(arg => {
+            prompt = prompt + arg.toString() + ' ';
+        });
+        return await this.runPrompt(prompt);
+    }
+}
