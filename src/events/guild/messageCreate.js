3415517db64a8c3004f9046778f48b39f0d4b757
Clément FOSSORIER <clementfiesta@gmail.com>
2023-10-08 20:39:59 +0200

    multi prefixes added

--- src/events/guild/messageCreate.js
+++ src/events/guild/messageCreate.js
@@ -1,23 +1,27 @@
 const promptController = require('../../controllers/promptController');
 
-const prefix = '-';
+const prefixes = ['-', '/', '!'];
 
 module.exports = {
     name: 'messageCreate',
     once: false,
     execute(client, message) {
         if(message.author.bot) return;
-        if(message.content.startsWith(prefix)) {
-            const args = message.content.slice(prefix.length).trim().split(/ +/g);
-            const commandName = args.shift().toLowerCase();
-            if(commandName.length == 0) return;
-    
-            const command = client.commands.get(commandName);
-            if(command) command.run(client, message, args);
-        }
+
+        prefixes.forEach(prefix => {
+            if(message.content.startsWith(prefix)) {
+                const args = message.content.slice(prefix.length).trim().split(/ +/g);
+                const commandName = args.shift().toLowerCase();
+                if(commandName.length == 0) return;
+        
+                const command = client.commands.get(commandName);
+                if(command) command.run(client, message, args);
+                return;
+            }
+        });
 
         if(message.mentions.has(client.user)) {
-            let prompt = message.content.replace(new RegExp(/<@&\d+>/g), '').trim();
+            let prompt = message.content.trim();
             promptController.execute(client, message, prompt);
         }
     }
