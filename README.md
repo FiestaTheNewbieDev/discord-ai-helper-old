037a780ed4277510e8dd4912057bb683995ad2e1
Clément FOSSORIER <clementfiesta@gmail.com>
2023-11-11 12:48:34 +0100

    Private args added to slash commands

--- README.md
+++ README.md
@@ -4,8 +4,6 @@
 AI Helper is an AI assistant Discord bot, it is based on OpenAI's GPT-4 and DALL-E models.
 AI Helper allows you to talk with it and generate images.
 
-
-
 ## Features
 - <b>Interactive Chat:</b> Interact with the @bot using natural language, powered by GPT-4 AI Model. The bot's response is optimized through carefully crafted prompts.
 - <b>Text Generation with GPT-4:</b> Use ```!gpt4 [prompt]``` command to generate text with GPT-4 AI Model. Your raw prompt is directly sent to the AI.
--- src/commands/gpt4.js
+++ src/commands/gpt4.js
@@ -9,6 +9,12 @@ module.exports = {
             description: 'Your prompt',
             required: true,
             type: 3
+        },
+        {
+            name: 'private',
+            'description': 'Make the command result private',
+            required: false,
+            type: 5
         }
     ],
     run: async (client, message, args) => {
@@ -16,7 +22,7 @@ module.exports = {
         message.channel.send(await gpt4Controller.runPromptWithArgs(args));
     },
     runSlash: async (client, interaction) => {
-        await interaction.deferReply({ephemeral: false}); 
+        await interaction.deferReply({ephemeral: interaction.options.getBoolean('private')}); 
         const args = interaction.options.getString('prompt');
         try {
             await interaction.editReply(await gpt4Controller.runPrompt(args));
