8714a856d5dc1b903e950caac7d0566c8e667917
Clément FOSSORIER <clementfiesta@gmail.com>
2023-11-11 13:08:21 +0100

    Image generation from interractive chat added

--- src/controllers/promptController.js
+++ src/controllers/promptController.js
@@ -1,9 +1,12 @@
 const gpt4Controller = require('./gpt4Controller');
+const dallEController = require('./dallEController');
 // const dallEController = require('./dallEController');
 
 module.exports = {
     async execute(client, message, prompt) {
-        prompt = `${message.author} send you this message: ${prompt}, respond to it following these instructions:\n
+        prompt = `${message.author} send you this message: '${prompt}', respond to it following these instructions:\n
+        - If you think that this request is a request to get an image start your response with PICTURE followed by tags list of the image\n
+        - Else respond by following the other instructions\n
         - you are ONLY ${client.user}\n
         - you are an interractive discord bot based on GPT-4 and DALL-E\n
         - ALWAYS speak with user's request language\n
@@ -11,18 +14,12 @@ module.exports = {
         `;
 
         message.channel.sendTyping();
-        message.channel.send(await gpt4Controller.runPrompt(prompt));
-
-        /*
-        prompt = await gpt4Controller.runPrompt(`If you think that this request "${userPrompt}" is a request to get an image say PICTURE, else say TEXT`);
-        switch(prompt) {
-            case 'TEXT':
-                message.channel.send(await gpt4Controller.runPrompt(`${message.author.username} send you this message: ${userPrompt}, respond to it following these instructions:\n- you are ${client.user.username}\n- you are an interractive discord bot based on GPT-4 and DALL-E\n- ALWAYS speak with user's request language\n- ALWAYS replace user name with ${message.author}`));
-                break;
-            case 'PICTURE':
-                message.channel.send(await dallEController.runPrompt(prompt));
-                break;
-        }
-        */
+        let response = await gpt4Controller.runPrompt(prompt);
+        if (response.startsWith('PICTURE')) {
+            message.channel.sendTyping();
+            response = response.replace('PICTURE', '');
+            response = response.replace(',', '');
+            message.channel.send(await dallEController.runPrompt(response));
+        } else message.channel.send(await gpt4Controller.runPrompt(prompt));
     }
 };
